d2082430bb253e36c5ea86bd4e70ba8dd28aa6f5
Marcus Hultman <marcus.hultman1@gmail.com>
2025-03-17 14:24:09 -0400

    main: make Value a type

--- main/main.cpp
+++ main/main.cpp
@@ -2,9 +2,13 @@
 #include <ranges>
 #include <string_view>
 #include <variant>
+#include <google/protobuf/any.pb.h>
+#include <google/protobuf/wrappers.pb.h>
 #include <range/v3/algorithm/contains.hpp>
 #include <range/v3/all.hpp>
 #include "linenoise.h"
+
+// util/trim.h
 
 std::string_view trimLeft(std::string_view str) {
   size_t start = str.find_first_not_of(" \t\n\r\f\v");
@@ -20,36 +24,63 @@ std::string_view trim(std::string_view str) {
   return trimLeft(trimRight(str));
 }
 
+// util/to_string_view.h
+
 auto toStringView(ranges::range auto &&s) -> std::string_view {
   return std::string_view(&*s.begin(), ranges::distance(s));
 }
 
-std::optional<ranges::any_view<std::string_view>> findBuiltin(std::string_view cmd) {
+// value.h
+
+struct Value {
+  using InnerValue = std::variant<google::protobuf::Any, google::protobuf::StringValue>;
+
+  Value(std::string_view str) {
+    google::protobuf::StringValue value;
+    value.set_value(str);
+    _value = value;
+  }
+
+  friend auto &operator<<(std::ostream &os, const Value &);
+
+  InnerValue _value;
+};
+
+auto &operator<<(std::ostream &os, const Value &v) {
+  return os << std::get<google::protobuf::StringValue>(v._value).value();
+}
+
+// builtin.h
+
+std::optional<ranges::any_view<Value>> findBuiltin(std::string_view cmd) {
   if (cmd.starts_with("iota")) {
-    return ranges::views::iota(1) | ranges::views::transform([](auto i) -> std::string_view {
-             return "sheep " + std::to_string(i);
-           });
+    return ranges::views::iota(1) |
+           ranges::views::transform([](auto i) { return Value("sheep " + std::to_string(i)); });
   }
   return {};
 }
 
+// command.h
+
 struct Command {
   Command(auto str) : _str{trim(std::string_view(str))} {}
 
-  auto run(ranges::range auto stream) -> ranges::any_view<std::string_view> {
+  auto run(ranges::range auto stream) -> ranges::any_view<Value> {
     if (_str.starts_with('{') && _str.ends_with('}')) {
-      return ranges::views::single("todo: closure");
+      return ranges::views::single(Value("todo: closure"));
     } else if (auto builtin = findBuiltin(_str)) {
       return *builtin;
     }
     // todo: parse parenthesis
     return _str | ranges::views::split_when([&](char c) { return ranges::contains(" \n", c); }) |
-           ranges::views::transform([](auto &&s) { return toStringView(s); });
+           ranges::views::transform([](auto &&s) { return Value(toStringView(s)); });
   }
 
  private:
   std::string_view _str;
 };
+
+// pipeline.h
 
 struct Pipeline {
   Pipeline(auto &&str, bool allow_stdout) : _allow_stdout{allow_stdout} {
@@ -72,7 +103,7 @@ struct Pipeline {
           std::cout << "[";
           for (auto &&[index, value] : ranges::views::enumerate(stream)) {
             // todo: format value
-            std::cout << (index ? ", " : "") << '"' << value << '"';
+            std::cout << (index ? ", " : "") << "\"" << value << "\"";
           }
           std::cout << "]" << std::endl;
         } else {
@@ -122,10 +153,12 @@ auto parsePipelines(std::string_view line) -> ranges::any_view<Pipeline> {
       ranges::views::single(Pipeline(line.substr(last_delim + 1), true)));
 }
 
+// main.cpp
+
 int main(int argc, char **argv) {
   for (const char *line; (line = linenoise("stream-shell v0.1 🚀> "));) {
     for (auto &&pipeline : parsePipelines(line)) {
-      ranges::any_view<std::string_view> stream = ranges::views::empty<std::string_view>;
+      ranges::any_view<Value> stream = ranges::views::empty<Value>;
       for (auto &&cmd : pipeline.cmds()) {
         stream = cmd.run(stream);
       }
