0f9fc0085d26359b3549b6b41fd47a740e84815a
Marcus Hultman <marcus.hultman1@gmail.com>
2025-04-13 14:49:07 -0400

    some more tests

--- stream-shell/test/stream_parser_test.cpp
+++ stream-shell/test/stream_parser_test.cpp
@@ -9,11 +9,18 @@ using namespace std::string_view_literals;
 
 constexpr auto each = boost::test_tools::per_element();
 
+bool operator==(const Result<Value> &value, bool b) {
+  return std::get<google::protobuf::Value>(*value).bool_value() == b;
+}
+
 bool operator==(const Result<Value> &value, double n) {
   return std::get<google::protobuf::Value>(*value).number_value() == n;
 }
+bool operator==(const Result<Value> &value, int n) {
+  return value == double(n);
+}
 
-bool operator==(const Result<Value> &value, const std::string &s) {
+bool operator==(const Result<Value> &value, std::string_view s) {
   return std::get<google::protobuf::Value>(*value).string_value() == s;
 }
 
@@ -49,9 +56,23 @@ BOOST_AUTO_TEST_CASE(empty) {
   BOOST_TEST(parse("").empty());
 }
 
-BOOST_AUTO_TEST_CASE(stream_expr) {
-  BOOST_TEST(parse("1 2 3") == std::vector<double>({1, 2, 3}), each);
-  BOOST_TEST(parse("\"foo\" + \"bar\"") == std::vector<std::string>({"foobar"}), each);
+BOOST_AUTO_TEST_CASE(stream_bools) {
+  BOOST_TEST(parse("true") == std::vector({true}), each);
+  BOOST_TEST(parse("false || false") == std::vector({false}), each);
+  BOOST_TEST(parse("true && false") == std::vector<bool>({false}), each);
+  BOOST_TEST(parse("true && !false") == std::vector<bool>({true}), each);
+}
+
+BOOST_AUTO_TEST_CASE(stream_numbers) {
+  BOOST_TEST(parse("1 + 2") == std::vector({3}), each);
+  BOOST_TEST(parse("1.5 - 0.75") == std::vector({.75}), each);
+  BOOST_TEST(parse("10 % 3") == std::vector({1}), each);
+  BOOST_TEST(parse("1 2 3") == std::vector({1, 2, 3}), each);
+  BOOST_TEST(parse("1 2..4 5") == std::vector({1, 2, 3, 4, 5}), each);
+}
+
+BOOST_AUTO_TEST_CASE(stream_strings) {
+  BOOST_TEST(parse("\"foo\" + \"bar\"") == std::vector({"foobar"sv}), each);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
