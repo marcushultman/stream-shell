621218b426e227b77cc48059e23cd0f6498e198c
Marcus Hultman <marcus.hultman1@gmail.com>
2025-04-13 20:18:50 -0400

    beef up the test suite

--- stream-shell/test/stream_parser_test.cpp
+++ stream-shell/test/stream_parser_test.cpp
@@ -1,43 +1,80 @@
 
 #include "stream-shell/stream_parser.h"
+#include <string>
+#include <vector>
 #include <boost/test/unit_test.hpp>
+#include <google/protobuf/struct.pb.h>
 #include <google/protobuf/util/json_util.h>
+#include <google/protobuf/util/message_differencer.h>
 #include <range/v3/all.hpp>
+#include "stream-shell/operand_op.h"
 #include "stream-shell/tokenize.h"
 
 using namespace std::string_view_literals;
 
 constexpr auto each = boost::test_tools::per_element();
 
-bool operator==(const Result<Value> &value, bool b) {
-  return std::get<google::protobuf::Value>(*value).bool_value() == b;
+namespace google::protobuf {
+
+bool operator==(const IsValue auto &lhs, const IsValue auto &rhs) {
+  return google::protobuf::util::MessageDifferencer::Equals(lhs, rhs);
 }
 
-bool operator==(const Result<Value> &value, double n) {
-  return std::get<google::protobuf::Value>(*value).number_value() == n;
-}
-bool operator==(const Result<Value> &value, int n) {
-  return value == double(n);
-}
-
-bool operator==(const Result<Value> &value, std::string_view s) {
-  return std::get<google::protobuf::Value>(*value).string_value() == s;
+auto &operator<<(std::ostream &os, const IsValue auto &value) {
+  std::string str;
+  BOOST_TEST(google::protobuf::util::MessageToJsonString(value, &str).ok());
+  return os << str;
 }
 
-auto &operator<<(std::ostream &os, const Result<Value> &value) {
-  if (value) {
-    std::string str;
-    std::visit(
-        [&](auto &value) {
-          BOOST_TEST(google::protobuf::util::MessageToJsonString(value, &str).ok());
-        },
-        *value);
-    return os << str;
-  }
-  return os << "Error " << int(value.error());
+auto &operator<<(std::ostream &os, const ::Value &value) {
+  return std::visit([&](auto &value) -> auto & { return os << value; }, value);
 }
+
+auto &operator<<(std::ostream &os, const Result<::Value> &value) {
+  return value ? (os << *value) : (os << "Error " << int(value.error()));
+}
+
+}  // namespace google::protobuf
 
 BOOST_AUTO_TEST_SUITE(strean_parser_test)
+
+google::protobuf::Value makeValue(bool b) {
+  google::protobuf::Value v;
+  v.set_bool_value(b);
+  return v;
+}
+
+google::protobuf::Value makeValue(int d) {
+  google::protobuf::Value v;
+  v.set_number_value(d);
+  return v;
+}
+
+google::protobuf::Value makeValue(double d) {
+  google::protobuf::Value v;
+  v.set_number_value(d);
+  return v;
+}
+
+google::protobuf::Value makeValue(std::string_view s) {
+  google::protobuf::Value v;
+  v.set_string_value(s);
+  return v;
+}
+
+struct JSON {
+  std::string str;
+};
+google::protobuf::Value makeValue(JSON json) {
+  google::protobuf::Value v;
+  BOOST_TEST(google::protobuf::json::JsonStringToMessage(json.str, v.mutable_struct_value()).ok());
+  return v;
+}
+
+template <typename... Args>
+std::vector<Value> makeValues(Args &&...args) {
+  return {makeValue(std::forward<Args>(args))...};
+}
 
 struct TestEnv : Env {
   StreamFactory getEnv(StreamRef) const override { return {}; }
@@ -56,23 +93,65 @@ BOOST_AUTO_TEST_CASE(empty) {
   BOOST_TEST(parse("").empty());
 }
 
-BOOST_AUTO_TEST_CASE(stream_bools) {
-  BOOST_TEST(parse("true") == std::vector({true}), each);
-  BOOST_TEST(parse("false || false") == std::vector({false}), each);
-  BOOST_TEST(parse("true && false") == std::vector<bool>({false}), each);
-  BOOST_TEST(parse("true && !false") == std::vector<bool>({true}), each);
+BOOST_AUTO_TEST_CASE(bools) {
+  BOOST_TEST(parse("true") == makeValues(true), each);
+  BOOST_TEST(parse("false || false") == makeValues(false), each);
+  BOOST_TEST(parse("true && false") == makeValues(false), each);
+  BOOST_TEST(parse("true && !false") == makeValues(true), each);
+  BOOST_TEST(parse("true !false true || true") == makeValues(true, true, true), each);
 }
 
-BOOST_AUTO_TEST_CASE(stream_numbers) {
-  BOOST_TEST(parse("1 + 2") == std::vector({3}), each);
-  BOOST_TEST(parse("1.5 - 0.75") == std::vector({.75}), each);
-  BOOST_TEST(parse("10 % 3") == std::vector({1}), each);
-  BOOST_TEST(parse("1 2 3") == std::vector({1, 2, 3}), each);
-  BOOST_TEST(parse("1 2..4 5") == std::vector({1, 2, 3, 4, 5}), each);
+BOOST_AUTO_TEST_CASE(numbers) {
+  BOOST_TEST(parse("1 + 2") == makeValues(3), each);
+  BOOST_TEST(parse("1.5 - 0.75") == makeValues(.75), each);
+  BOOST_TEST(parse("10 % 3") == makeValues(1), each);
+  BOOST_TEST(parse("1 2 3") == makeValues(1, 2, 3), each);
+  BOOST_TEST(parse("1 2..4 5") == makeValues(1, 2, 3, 4, 5), each);
 }
 
-BOOST_AUTO_TEST_CASE(stream_strings) {
-  BOOST_TEST(parse("\"foo\" + \"bar\"") == std::vector({"foobar"sv}), each);
+BOOST_AUTO_TEST_CASE(strings) {
+  BOOST_TEST(parse("'foo' + 'bar'") == makeValues("foobar"sv), each);
+  BOOST_TEST(parse("\"foo\" + \"bar\"") == makeValues("foobar"sv), each);
+  BOOST_TEST(parse("`foo` + `bar`") == makeValues("foobar"sv), each);
+  // todo: fix double negation
+  // BOOST_TEST(parse("!!`foo` !`foo`") == makeValues(true, false), each);
+  // todo: stop using json for string interpolation
+  // BOOST_TEST(parse("FOO=(1..3) `foo $FOO`") == makeValues("foo 1 2 3"sv), each);
+}
+
+BOOST_AUTO_TEST_CASE(parentheses) {
+  BOOST_TEST(parse("1 -2 + 3") == makeValues(2), each);
+  BOOST_TEST(parse("1 (-2 + 3)") == makeValues(1, 1), each);
+  BOOST_TEST(parse("(1 -2)(+3)") == makeValues(-1, 3), each);
+}
+
+BOOST_AUTO_TEST_CASE(pipe) {
+  BOOST_TEST(parse("1 | 2") == makeValues(2), each);
+  BOOST_TEST(parse("1.. | 2 3") == makeValues(2, 3), each);
+  BOOST_TEST(parse("1 | ({ name: `Bernard` })") == makeValues(JSON("{ name: \"Bernard\" }")), each);
+}
+
+BOOST_AUTO_TEST_CASE(json) {
+  BOOST_TEST(parse("{ name: `Bernard` }") == makeValues(JSON("{ name: \"Bernard\" }")), each);
+}
+
+BOOST_AUTO_TEST_CASE(closure) {
+  // todo: add builtin
+  // BOOST_TEST(parse("1..2 | { add 1 }") == makeValues(2, 3), each);
+  BOOST_TEST(parse("1..2 | { i -> i i * 2 }") == makeValues(1, 2, 2, 4), each);
+  BOOST_TEST(parse(R"cmd(
+    { name: `Albert` }
+    { name: `Bernard` }
+    | { person -> person.name })cmd") == makeValues("Albert"sv, "Bernard"sv),
+             each);
+  BOOST_TEST(parse("{ numbers: [1, 2] } { numbers: [3, 4] } | { e -> e.numbers }") ==
+                 makeValues(1, 2, 3, 4),
+             each);
+}
+
+BOOST_AUTO_TEST_CASE(builtins) {
+  // todo: fake exit
+  // BOOST_TEST(parse("exit") == makeValues(2, 3), each);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
