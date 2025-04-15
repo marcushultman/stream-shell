ab175251d50cb9e6890040d33b5470c050a4a7a8
Marcus Hultman <marcus.hultman1@gmail.com>
2025-04-15 14:06:22 -0400

    read envvar on the fly

--- stream-shell/repl.h
+++ stream-shell/repl.h
@@ -11,41 +11,32 @@ extern char **environ;
 
 using namespace std::string_view_literals;
 
-static auto kEnvVars =
-    ranges::views::iota(0) | ranges::views::transform([](auto i) { return environ[i]; }) |
-    ranges::views::take_while(std::identity()) |
-    ranges::views::transform([](auto var) { return std::string_view(var); }) |
-    ranges::views::transform([](auto var) { return ranges::views::concat("$"sv, var); }) |
-    ranges::views::transform([](auto var) { return tokenize(var); }) | ranges::to<std::vector>;
-
 // env.h
 struct ProdEnv final : Env {
   ProdEnv() {
-    auto env_parser = makeStreamParser(*this);
-
-    for (auto &expr : kEnvVars) {
-      env_parser->parse(expr);
-    }
-
     if (auto *xdg_data_home_dir = getenv("XDG_DATA_HOME")) {
-      load(*env_parser, xdg_data_home_dir);
+      load(xdg_data_home_dir);
     } else if (auto *home_dir = getenv("HOME")) {
-      load(*env_parser, std::string(home_dir) + "/.config");
+      load(std::string(home_dir) + "/.config");
     }
   }
 
   StreamFactory getEnv(StreamRef ref) const override {
-    if (auto it = _env_vars.find(ref); it != _env_vars.end()) {
+    if (auto it = _cache.find(ref); it != _cache.end()) {
       return it->second;
+    } else if (auto var = std::getenv((ref.name | ranges::to<std::string>).c_str())) {
+      auto [stream, _] = _parser->parse(tokenize(std::string_view(var)));
+      return _cache[ref] = [stream] { return stream; };
     }
     return {};
   }
   void setEnv(StreamRef ref, StreamFactory stream) override {
-    _env_vars.emplace(ref, std::move(stream));
+    // todo: setenv?
+    _cache[ref] = std::move(stream);
   }
 
  private:
-  void load(StreamParser &parser, std::string path) {
+  void load(std::string path) {
     std::ifstream config(path + "/stream-shell/config.st", std::ios::in);
     if (!config.is_open()) {
       return;
@@ -53,11 +44,12 @@ struct ProdEnv final : Env {
     std::stringstream buffer;
     buffer << config.rdbuf();
     _config = buffer.str();
-    (void)parser.parse(tokenize(_config));
+    (void)_parser->parse(tokenize(_config));
   }
 
   std::string _config;
-  std::map<StreamRef, StreamFactory, std::less<>> _env_vars;
+  std::unique_ptr<StreamParser> _parser = makeStreamParser(*this);
+  mutable std::map<StreamRef, StreamFactory, std::less<>> _cache;
 };
 
 inline void repl(Prompt prompt) {
