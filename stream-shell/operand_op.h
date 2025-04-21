91cc23826b44e6c69c801e51bf3665e824171f99
Marcus Hultman <marcus.hultman1@gmail.com>
2025-04-20 21:52:38 -0400

    transform Stream in the same ValueTransform fn

--- stream-shell/operand_op.h
+++ stream-shell/operand_op.h
@@ -62,7 +62,7 @@ struct Iota {
 };
 
 struct Background {
-  auto operator()(Stream s) {
+  auto operator()(const auto &) {
     // todo: implement backgrounding
     return std::unexpected(Error::kInvalidOp);
   }
@@ -71,102 +71,89 @@ struct Background {
 /**
  * Visits Operand variants transforming Values. For use in operators and builins
  */
-template <typename ValueT, typename StreamT = std::nullptr_t>
+template <typename ValueT>
 struct ValueTransform {
-  ValueTransform(ValueT &&t) : _value_t(std::move(t)) {}
-
-  ValueTransform(ValueT &&value_t, StreamT &&stream_t)
-      : _value_t(std::move(value_t)), _stream_t(std::move(stream_t)) {}
+  ValueTransform(ValueT &&value_t) : _value_t(std::move(value_t)) {}
 
   auto operator()(const IsExprValue auto &...v) -> Operand {
-    if (auto result = eval1(v...)) {
+    if (auto result = _value_t(v...)) {
       return std::visit([](auto value) -> Operand { return value; }, *result);
     } else {
       return [result](auto &) { return result; };
     }
   }
-  auto operator()(const auto &...v) -> Operand { return eval2(v...); }
+  auto operator()(const auto &...v) -> Operand { return eval(v...); }
 
  private:
-  auto eval1(const Stream &v) -> Expr::result_type { return _stream_t(v); }
-  auto eval1(const IsExprValue auto &...v) -> Expr::result_type { return _value_t(v...); }
-
-  auto eval2(const Expr &v) -> Expr {
-    return [op = *this, v](const Closure &closure) mutable {
+  auto eval(const Expr &v) -> Expr {
+    return [value_t = _value_t, v](const Closure &closure) mutable {
       return v(closure).and_then(
-          [&](auto v) { return std::visit([&](auto v) { return op.eval1(v); }, v); });
+          [&](auto v) { return std::visit([&](auto v) { return value_t(v); }, v); });
     };
   }
 
-  auto eval2(const Expr &lhs, const IsValue auto &rhs) -> Expr {
-    return [lhs, op = *this, rhs](const Closure &closure) mutable {
+  auto eval(const Expr &lhs, const IsValue auto &rhs) -> Expr {
+    return [lhs, value_t = _value_t, rhs](const Closure &closure) mutable {
       return lhs(closure)
           .and_then([&](auto lhs) {
-            return std::visit([&](IsExprValue auto lhs) { return op.eval1(lhs, rhs); }, lhs);
+            return std::visit([&](IsExprValue auto lhs) { return value_t(lhs, rhs); }, lhs);
           })
-          .or_else([&](Error err) { return op._value_t(err, rhs); });
+          .or_else([&](Error err) { return value_t(err, rhs); });
     };
   }
-  auto eval2(const IsValue auto &lhs, const Expr &rhs) -> Expr {
-    return [lhs, op = *this, rhs](const Closure &closure) mutable {
+  auto eval(const IsValue auto &lhs, const Expr &rhs) -> Expr {
+    return [lhs, value_t = _value_t, rhs](const Closure &closure) mutable {
       return rhs(closure).and_then(
-          [&](auto rhs) { return std::visit([&](auto &rhs) { return op.eval1(lhs, rhs); }, rhs); });
+          [&](auto rhs) { return std::visit([&](auto &rhs) { return value_t(lhs, rhs); }, rhs); });
     };
   }
-  auto eval2(const Expr &lhs, const Expr &rhs) -> Expr {
+  auto eval(const Expr &lhs, const Expr &rhs) -> Expr {
     return [lhs, op = *this, rhs](const Closure &closure) mutable {
       return rhs(closure).and_then([&](auto rhs) {
-        return std::visit([&](auto rhs) { return op.eval2(lhs, rhs)(closure); }, rhs);
+        return std::visit([&](auto rhs) { return op.eval(lhs, rhs)(closure); }, rhs);
       });
     };
   }
-  auto eval2(const auto &...) -> Expr {
+  auto eval(const auto &...) -> Expr {
     return [](auto &) { return std::unexpected(Error::kInvalidOp); };
   }
 
   ValueT _value_t;
-  StreamT _stream_t = {};
 };
 
 struct OperandOp {
   OperandOp(Token op) : op{op} {}
 
   auto operator()(const auto &v) const -> Operand {
-    return ValueTransform(
-        [&](const auto &v) -> Expr::result_type {
-          if (op == "+") return ValueOp<std::identity>()(v);
-          if (op == "-") return ValueOp<std::negate<>>()(v);
-          if (op == "!") return ValueOp<std::logical_not<>, bool>()(v);
-          if (op == "..") return ValueOp<Iota>()(v);
-          return std::unexpected(Error::kInvalidOp);
-        },
-        [&](const Stream &s) -> Expr::result_type {
-          if (op == "&") return Background()(s);
-          return std::unexpected(Error::kInvalidOp);
-        })(v);
+    return ValueTransform([&](const auto &v) -> Expr::result_type {
+      if (op == "+") return ValueOp<std::identity>()(v);
+      if (op == "-") return ValueOp<std::negate<>>()(v);
+      if (op == "!") return ValueOp<std::logical_not<>, bool>()(v);
+      if (op == "..") return ValueOp<Iota>()(v);
+      if (op == "&") return Background()(v);
+      return std::unexpected(Error::kInvalidOp);
+    })(v);
   }
   auto operator()(const auto &...v) const -> Operand {
-    return ValueTransform(
-        [op = op](const auto &...v) -> Expr::result_type {
-          if (op == "||") return ValueOp<std::logical_or<>, bool>()(v...);
-          if (op == "&&") return ValueOp<std::logical_and<>, bool>()(v...);
-          if (op == "==") return ValueOp<std::equal_to<>, bool>()(v...);
-          if (op == "!=") return ValueOp<std::not_equal_to<>, bool>()(v...);
-          if (op == "<") return ValueOp<std::less<>, bool>()(v...);
-          if (op == "<=") return ValueOp<std::less_equal<>, bool>()(v...);
-          if (op == ">") return ValueOp<std::greater<>, bool>()(v...);
-          if (op == ">=") return ValueOp<std::greater_equal<>, bool>()(v...);
-          if (op == "+") return ValueOp<std::plus<>>()(v...);
-          if (op == "-") return ValueOp<std::minus<>>()(v...);
-          if (op == "*") return ValueOp<std::multiplies<>>()(v...);
-          if (op == "/") return ValueOp<std::divides<>>()(v...);
-          if (op == "%") return ValueOp<std::modulus<>>()(v...);
-          if (op == "?") return TernaryCondition()(v...);
-          if (op == "?:") return TernaryEvaluation()(v...);
-          if (op == "..") return ValueOp<Iota>()(v...);
-          return std::unexpected(Error::kInvalidOp);
-        },
-        {})(v...);
+    return ValueTransform([op = op](const auto &...v) -> Expr::result_type {
+      if (op == "||") return ValueOp<std::logical_or<>, bool>()(v...);
+      if (op == "&&") return ValueOp<std::logical_and<>, bool>()(v...);
+      if (op == "==") return ValueOp<std::equal_to<>, bool>()(v...);
+      if (op == "!=") return ValueOp<std::not_equal_to<>, bool>()(v...);
+      if (op == "<") return ValueOp<std::less<>, bool>()(v...);
+      if (op == "<=") return ValueOp<std::less_equal<>, bool>()(v...);
+      if (op == ">") return ValueOp<std::greater<>, bool>()(v...);
+      if (op == ">=") return ValueOp<std::greater_equal<>, bool>()(v...);
+      if (op == "+") return ValueOp<std::plus<>>()(v...);
+      if (op == "-") return ValueOp<std::minus<>>()(v...);
+      if (op == "*") return ValueOp<std::multiplies<>>()(v...);
+      if (op == "/") return ValueOp<std::divides<>>()(v...);
+      if (op == "%") return ValueOp<std::modulus<>>()(v...);
+      if (op == "?") return TernaryCondition()(v...);
+      if (op == "?:") return TernaryEvaluation()(v...);
+      if (op == "..") return ValueOp<Iota>()(v...);
+      return std::unexpected(Error::kInvalidOp);
+    })(v...);
   }
 
  private:
